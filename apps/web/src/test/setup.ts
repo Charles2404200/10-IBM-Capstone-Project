b7a1a28757854002afc3480023fb7e61b98333c4
Christine Le <lechristine2004@gmail.com>
2026-09-04 18:11:25 +1000

    test: add form validation, admin user creation, and state rendering tests

--- apps/web/src/test/setup.ts
+++ apps/web/src/test/setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom'
+
+// provide ResizeObserver for Carbon components that require it in jsdom
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.ResizeObserver = ResizeObserver
